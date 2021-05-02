9d4428aacdcbb0e3781a1fbd71d1a6ba5c122d20
Wkun1996 <wk1996@icloud.com>
2021-05-02 21:47:01 +0800

    完成AbstractList源码的阅读

--- src/main/java/cn/xiaohupao/list/arraylist/MyAbstractList.java
+++ src/main/java/cn/xiaohupao/list/arraylist/MyAbstractList.java
@@ -416,5 +416,306 @@ public abstract class MyAbstractList<E> extends AbstractCollection<E> implements
     private String outOfBoundsMsg(int index){
         return "Index: " + index + ", Size: " + size();
     }
+}
 
+/**
+ * 用于截取list中的元素作为子序列
+ * @param <E> 子序列的范型
+ */
+class SubList<E> extends MyAbstractList<E>{
+    /**
+     * 传入的集合类
+     */
+    private final MyAbstractList<E> l;
+    /**
+     * 偏移量
+     */
+    private final int offset;
+    /**
+     * 截取的元素size
+     */
+    private int size;
+
+    /**
+     * 子序列的构造方法
+     * @param list 传入的集合
+     * @param fromIndex 其实索引
+     * @param toIndex 结束索引
+     */
+    SubList(MyAbstractList<E> list, int fromIndex, int toIndex){
+        if (fromIndex < 0){
+            throw new IndexOutOfBoundsException("fromIndex = " + fromIndex);
+        }
+        if (toIndex > list.size()){
+            throw new IndexOutOfBoundsException("toIndex = " + toIndex);
+        }
+        if (fromIndex > toIndex){
+            throw new IllegalArgumentException("fromIndex(" + fromIndex +
+                    ") > toIndex(" + toIndex + ")");
+        }
+
+        l = list;
+        offset = fromIndex;
+        size = toIndex - fromIndex;
+        this.modCount = l.modCount;
+    }
+
+    /**
+     * 修改指定索引处子序列的索引
+     * @param index 指定的元素
+     * @param element 新的元素
+     * @return
+     */
+    @Override
+    public E set(int index, E element){
+        rangeCheck(index);
+        checkForComodification();
+        return l.set(index+offset, element);
+    }
+
+    /**
+     * 获得子学列指定索引处的元素
+     * @param index 指定的索引
+     * @return 指定索引处的元素
+     */
+    @Override
+    public E get(int index) {
+        rangeCheck(index);
+        checkForComodification();
+        return l.get(index+offset);
+    }
+
+    /**
+     * 子序列的元素个数
+     * @return 子序列的元素个数
+     */
+    @Override
+    public int size() {
+        checkForComodification();
+        return size;
+    }
+
+    /**
+     * 在索引位置添加元素
+     * @param index 指定的索引位置
+     * @param element 添加的元素
+     */
+    @Override
+    public void add(int index, E element){
+        rangeCheckForAdd(index);
+        checkForComodification();
+        l.add(index+offset, element);
+        this.modCount = l.modCount;
+        size++;
+    }
+
+    /**
+     * 移除指定索引位置的元素
+     * @param index 指定的索引位置
+     * @return
+     */
+    @Override
+    public E remove(int index){
+        rangeCheck(index);
+        checkForComodification();
+        E result = l.remove(index+offset);
+        this.modCount = l.modCount;
+        return result;
+    }
+
+    /**
+     * 在子序列中移除从起始索引位置到结束位置的元素(不包括结束位置)
+     * @param fromIndex 起始索引
+     * @param toIndex 结束索引
+     */
+    @Override
+    protected void removeRange(int fromIndex, int toIndex){
+        checkForComodification();
+        l.removeRange(fromIndex+offset, toIndex+offset);
+        this.modCount = l.modCount;
+        size -= (toIndex - fromIndex);
+    }
+
+    /**
+     * 在子学列中添加传入集合中的元素
+     * @param c 指定的集合
+     * @return true则表示添加成功
+     */
+    @Override
+    public boolean addAll(Collection<? extends E> c){
+        return addAll(size, c);
+    }
+
+    /**
+     * 在子序列中在指定索引位置传入集合中的元素
+     * @param index 指定的索引
+     * @param c 指定的集合
+     * @return
+     */
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c){
+        rangeCheckForAdd(index);
+        int cSize = c.size();
+        if (cSize==0){
+            return false;
+        }
+        checkForComodification();
+        l.addAll(offset+index, c);
+        this.modCount = l.modCount;
+        size += cSize;
+        return true;
+    }
+
+    /**
+     * 获取列表迭代器
+     * @return 列表迭代器
+     */
+    @Override
+    public Iterator<E> iterator(){
+        return listIterator();
+    }
+
+    /**
+     * 在指定索引位置处获取列表迭代器
+     * @param index 迭代器所在的索引位置
+     * @return 列表迭代器
+     */
+    @Override
+    public ListIterator<E> listIterator(final int index){
+        checkForComodification();
+        rangeCheckForAdd(index);
+
+        return new ListIterator<E>() {
+            private final ListIterator<E> i = l.listIterator(index+offset);
+
+            /**
+             * 判断是否还有下一个元素
+             * @return true则表示还有下一个元素
+             */
+            @Override
+            public boolean hasNext() {
+                return nextIndex() < size;
+            }
+
+            /**
+             * 获取下一个元素
+             * @return 下一个元素
+             */
+            @Override
+            public E next() {
+                if (hasNext()) {
+                    return i.next();
+                }else {
+                    throw new NoSuchElementException();
+                }
+            }
+
+            /**
+             * 判断是否还有上一个元素
+             * @return true则表示还有上一个元素
+             */
+            @Override
+            public boolean hasPrevious() {
+                return previousIndex() >= 0;
+            }
+
+            /**
+             * 获取迭代器前一个元素
+             * @return 迭代器前一个元素
+             */
+            @Override
+            public E previous() {
+                if (hasPrevious()){
+                    return i.previous();
+                }else{
+                    throw new NoSuchElementException();
+                }
+            }
+
+            /**
+             * 获取迭代器下一个索引位置
+             * @return 下一个索引
+             */
+            @Override
+            public int nextIndex() {
+                return i.nextIndex() - offset;
+            }
+
+            /**
+             * 获取迭代器上一个索引位置
+             * @return 上一个索引位置
+             */
+            @Override
+            public int previousIndex() {
+                return i.previousIndex() - offset;
+            }
+
+            /**
+             * 移除迭代器当前位置的元素
+             */
+            @Override
+            public void remove() {
+                i.remove();
+                SubList.this.modCount = l.modCount;
+                size--;
+            }
+
+            /**
+             * 设置迭代器当前位置的元素
+             * @param e 指定的元素
+             */
+            @Override
+            public void set(E e) {
+                i.set(e);
+                SubList.this.modCount = l.modCount;
+            }
+
+            /**
+             * 在迭代器当前位置添加元素
+             * @param e 指定的元素
+             */
+            @Override
+            public void add(E e) {
+                i.add(e);
+                SubList.this.modCount = l.modCount;
+                size++;
+            }
+        };
+    }
+    @Override
+    public List<E> subList(int fromIndex, int toIndex){
+        return new SubList<>(this, fromIndex, toIndex);
+    }
+
+    private void rangeCheck(int index){
+        if (index < 0 || index >= size){
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+        }
+    }
+
+    private void rangeCheckForAdd(int index){
+        if (index < 0 || index > size){
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+        }
+    }
+
+    private String outOfBoundsMsg(int index){
+        return "Index: " + index + ", Size: " + size;
+    }
+
+    private void checkForComodification(){
+        if (this.modCount != l.modCount){
+            throw new ConcurrentModificationException();
+        }
+    }
+}
+
+/**
+ * 继承了SubList并实现了随机访问接口
+ * @param <E> 范型的类型
+ */
+class RandomAccessSubList<E> extends SubList<E> implements RandomAccess{
+    RandomAccessSubList(MyAbstractList<E> list, int fromIndex, int toIndex){
+        super(list, fromIndex, toIndex);
+    }
 }
